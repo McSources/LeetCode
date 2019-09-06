02aacb7ab141c8802a7e1ddfe87cda9efa5aa767
machao <machao1996@live.com>
2019-09-06 13:39:56 +0800

    第十三题

---
+++ src/main/java/leetcode/no13easy/No13Test.java
@@ -0,0 +1,8 @@
+package leetcode.no13easy;
+
+public class No13Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.romanToInt("MCMXCIV"));
+    }
+}
