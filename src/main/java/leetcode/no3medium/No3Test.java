609e551a144a27985bfcd4b3a1c321d3180aca5c
machao <machao1996@live.com>
2020-03-05 13:01:39 +0800

    无重复字符的最长子串

---
+++ src/main/java/leetcode/no3medium/No3Test.java
@@ -0,0 +1,12 @@
+package leetcode.no3medium;
+
+/**
+ * @author machao
+ * @date 2020/3/2
+ */
+public class No3Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.lengthOfLongestSubstring("bbbbbbb"));
+    }
+}
--- src/main/java/leetcode/util/Timer.java
+++ src/main/java/leetcode/util/Timer.java
@@ -1,23 +1,30 @@
 package leetcode.util;
 
 /**
+ * 计时器
+ * TODO
+ * <ol>
+ *     <li>允许设置时间单位</li>
+ *     <li>允许选择，程序执行中打印日志还是执行结束之后统一打印日志，需要有一个list记录时间和日志信息</li>
+ * </ol>
+ *
  * @author machao
  * @date 2019/12/26
  */
 public class Timer {
-    private volatile long startTime;
-    private volatile long endTime;
+    private long startTime;
 
+    /**
+     * 开始计时，设置开始时间
+     */
     public void start() {
         startTime = System.currentTimeMillis();
     }
 
-    public void end() {
-        endTime = System.currentTimeMillis();
-        System.err.println(endTime - startTime);
-    }
-
-    public long duration() {
-        return endTime - startTime;
+    /**
+     * 计算间隔，打印花费的时间
+     */
+    public void duration() {
+        System.err.println(System.currentTimeMillis() - startTime);
     }
 }
