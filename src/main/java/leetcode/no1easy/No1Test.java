b300f0be42c58a47b159d14afc98ded3da92b7a0
machao <machao1996@live.com>
2020-11-02 20:25:48 +0800

    去掉计时器

--- src/main/java/leetcode/no1easy/No1Test.java
+++ src/main/java/leetcode/no1easy/No1Test.java
@@ -1,7 +1,5 @@
 package leetcode.no1easy;
 
-import leetcode.util.Timer;
-
 import java.util.Arrays;
 
 class No1Test {
@@ -9,10 +7,7 @@ class No1Test {
         Solution solution = new Solution();
         int[] nums = new int[]{3, 7, 11, 6};
         int target = 9;
-        Timer timer = new Timer();
-        timer.start();
         int[] result = solution.twoSum2(nums, target);
-        timer.end();
         System.out.println(Arrays.toString(result));
     }
 }
