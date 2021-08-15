aad90c2f71f1ebd03d517861bdf65042b6739333
machao <machao1996@live.com>
2021-08-15 18:27:26 +0800

    重新组织整理

---
+++ src/main/java/leetcode/mypractice/Practice1.java
@@ -0,0 +1,33 @@
+package leetcode.mypractice;
+
+/**
+ * 0-25对应A-Z
+ * 26-52对应AA-AZ
+ * 53-72对应BA-BZ
+ * ……
+ * 以此类推，求数字i对应的字母。
+ *
+ * @author machao
+ * @date 2021/8/11
+ */
+public class Practice1 {
+    public static void main(String[] args) {
+
+    }
+
+    public String solution(int num) {
+//        int i = num;
+//        int x = 26;
+//        int len = 1;
+//        while(i>=x){
+//            i = i-x;
+//            x = x*x;
+//            len+=1;
+//        }
+        return null;
+    }
+
+    public char[] chars() {
+        return new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    }
+}
---
+++ src/main/java/leetcode/mypractice/Practice2.java
@@ -0,0 +1,18 @@
+package leetcode.mypractice;
+
+/**
+ * 求两个字符串的最长公共子串，输入："cdsjhk"，"sdcdsjbk"，返回"cdsj"。
+ *
+ * @author machao
+ * @date 2021/8/11
+ */
+public class Practice2 {
+    public static void main(String[] args) {
+
+    }
+
+    public String solution(String a, String b) {
+
+        return null;
+    }
+}
